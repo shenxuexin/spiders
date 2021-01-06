3e3b58706bff97283ed59133b0b1298c3dcc7d6a
shenxuexin <shenxuexin@ruc.edu.cn>
2021-01-06 11:28:52 +0800

    1. 更新目录结构 2. 修复文件存储路径错误 3. 添加部分报警和统计信息

--- music163/music163_spider.py
+++ music163/music163_spider.py
@@ -9,7 +9,9 @@ import requests
 import os
 import time
 import sys
+import re
 
+from datetime import datetime
 from threading import Thread
 from queue import Queue
 from lxml import etree
@@ -26,7 +28,8 @@ class MusicSpider(object):
         self.singer_batch_link_Queue = Queue()
         self.singer_queue = Queue()
         self.song_queue = Queue()
-        self.file_save_path = os.path.join(FILE_SAVE_PATH, self.mode)
+        self.file_save_path = os.path.join(FILE_SAVE_PATH, self.type)
+        self.count = {'category': 0, 'singer': 0, 'song': 0}
 
         self.session.headers.update({'User-Agent': USER_AGENT})
 
@@ -36,6 +39,11 @@ class MusicSpider(object):
 
         if not os.path.exists(self.file_save_path):
             os.makedirs(self.file_save_path)
+
+    @staticmethod
+    def regular_file_str(str):
+        # 文件名不能包含 \/:*?"<>字符
+        return re.sub(r'[\\/:*?"<>|]', '_', str)
 
     def safe_get(self, url, params=None):
         time.sleep(SLEEP_PER_REQUEST)
@@ -63,11 +71,14 @@ class MusicSpider(object):
 
         category_list = []
         for cate_elem in cate_elem_list:
-            name = cate_elem.xpath('./a/text()')[0] if cate_elem.xpath('./a/text()') else None
+            cate_name = cate_elem.xpath('./a/text()')[0] if cate_elem.xpath('./a/text()') else None
             url = BASIC_SERVER_URL + cate_elem.xpath('./a/@href')[0] if cate_elem.xpath('./a/@href') else None
-            category_list.append(name)
 
-            self.category_queue.put((name, url))
+            if cate_name:
+                cate_name = self.regular_file_str(cate_name)
+                category_list.append(cate_name)
+                self.count['category'] += 1
+                self.category_queue.put((cate_name, url))
 
         logger.info(f'Get category successful. | category_list: {category_list}')
 
@@ -88,16 +99,15 @@ class MusicSpider(object):
             for link_elem in link_elem_list:
                 url = BASIC_SERVER_URL + link_elem.xpath('./@href')[0] if link_elem.xpath('./@href') else None
                 if url:
-                    self.singer_batch_link_Queue.put(url)
+                    self.singer_batch_link_Queue.put((cate, url))
 
             logger.info(f'Get singer batch link done.')
             self.category_queue.task_done()
-            i += 1
 
     def get_singer_list(self):
         while True:
             if not self.singer_batch_link_Queue.empty():
-                singer_batch_url = self.singer_batch_link_Queue.get()
+                cate, singer_batch_url = self.singer_batch_link_Queue.get()
             else:
                 time.sleep(3)
                 continue
@@ -107,11 +117,14 @@ class MusicSpider(object):
                 continue
 
             html = etree.HTML(resp.text)
-            singer_elem_list = html.xpath('//ul[@id="m-artist-box"]/li')
+            singer_elem_list = html.xpath('//ul[@id="m-artist-box"]/li//a[@class="f-tdn"]/preceding-sibling::a')
             for singer_elem in singer_elem_list:
-                url = BASIC_SERVER_URL + singer_elem.xpath('.//a[@class="f-tdn"]/preceding-sibling::a/@href')[0].strip() if singer_elem.xpath('.//a[@class="f-tdn"]/preceding-sibling::a/@href') else None
-                if url:
-                    self.singer_queue.put(url)
+                singer_name = singer_elem.xpath('./text()')[0].strip() if singer_elem.xpath('./text()') else None
+                url = BASIC_SERVER_URL + singer_elem.xpath('./@href')[0].strip() if singer_elem.xpath('./@href') else None
+                if all([singer_name, url]):
+                    singer_name = self.regular_file_str(singer_name)
+                    self.count['singer'] += 1
+                    self.singer_queue.put((cate, singer_name, url))
 
             logger.info(f'Get singer list done. | url: {singer_batch_url}')
             self.singer_batch_link_Queue.task_done()
@@ -119,7 +132,7 @@ class MusicSpider(object):
     def get_song_list(self):
         while True:
             if not self.singer_queue.empty():
-                singer_url = self.singer_queue.get()
+                cate, singer_name, singer_url = self.singer_queue.get()
             else:
                 time.sleep(3)
                 continue
@@ -136,8 +149,10 @@ class MusicSpider(object):
                 title = song_elem.xpath('./text()')[0] if song_elem.xpath('./text()') else None
 
                 if all([url, title]):
+                    title = self.regular_file_str(title)
                     song_id = url.split('?')[-1].split('=')[-1]
-                    self.song_queue.put((title, song_id))
+                    self.count['song'] += 1
+                    self.song_queue.put((cate, singer_name, title, song_id))
                 else:
                     logger.warning(f'url: {url}, title: {title}')
 
@@ -147,12 +162,12 @@ class MusicSpider(object):
     def song_download(self):
         while True:
             if not self.song_queue.empty():
-                title, song_id = self.song_queue.get(timeout=1)
+                cate, singer_name, title, song_id = self.song_queue.get(timeout=1)
             else:
                 time.sleep(3)
                 continue
 
-            logger.info(f'Start downloading. | title: {title}')
+            logger.info(f'Start downloading. | singer: {singer_name} | title: {title}')
             download_url = SONG_DOWNLOAD_URL.format(song_id)
             resp = self.safe_get(download_url)
 
@@ -160,16 +175,29 @@ class MusicSpider(object):
                 self.song_queue.task_done()
                 continue
 
-            save_path = os.path.join(self.file_save_path, f'{title}.mp3')
-            if os.path.exists(save_path):
-                logger.info(f'file already exists. | title: {title}')
-            with open(save_path, 'wb') as f:
-                f.write(resp.content)
+            singer_folder = os.path.join(self.file_save_path, cate, singer_name)
+            if not os.path.exists(singer_folder):
+                os.makedirs(singer_folder)
 
-            self.song_queue.task_done()
-            logger.info(f'Download successful. | title: {title}')
+            save_path = os.path.join(singer_folder, f'{title}.mp3')
+            if os.path.exists(save_path):
+                logger.info(f'file already exists. | singer: {singer_name} | title: {title}')
+                self.song_queue.task_done()
+                continue
+
+            try:
+                with open(save_path, 'wb') as f:
+                    f.write(resp.content)
+            except Exception as e:
+                logger.error(f'Save file failed. | Exception: {e}')
+            else:
+                logger.info(f'Download successful. | singer: {singer_name} | title: {title}')
+            finally:
+                self.song_queue.task_done()
 
     def main(self):
+        time_bgn = datetime.now().replace(microsecond=0)
+        logger.info(f'Start scraping. | time: {time_bgn}')
         self.get_category()
         t_get_singer_batch_link = Thread(target=self.get_singer_batch_link)
         t_get_singer_list = Thread(target=self.get_singer_list)
@@ -183,7 +211,7 @@ class MusicSpider(object):
         for q in [self.category_queue, self.singer_batch_link_Queue, self.singer_queue, self.song_queue]:
             q.join()
 
-        logger.info(f'Scrape done.')
+        logger.info(f'Scrape done. | count: {self.count} | time_span: {(datetime.now()-time_bgn).seconds}s')
 
 
 if __name__ == '__main__':
